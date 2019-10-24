796a7ebbaea4b0eb02f250e8e9ca4b7d71a8f674
Pevnak <pevnak@gmail.com>
2019-10-24 09:27:27 +0200

    added a test for extractonehot and extended its documentation

--- src/extractors/extractonehot.jl
+++ src/extractors/extractonehot.jl
@@ -1,9 +1,42 @@
 using SparseArrays, FillArrays, Mill
 
 """
+	ExtractOneHot(ks, k, v)
 
-	Converts a Vector of `Dict` items to one-hot encoding. 
+	Converts a Vector of `Dict` items to one-hot encoding by using key 
+	`k` to identify a name of item out of `ks` and values 
+	of key `v` as values. 
 
+```juliadoctest
+julia> samples = ["{\"name\": \"a\", \"count\" : 1}",
+		"{\"name\": \"b\", \"count\" : 2}",];
+julia> samples = JSON.parse.(samples);
+julia> e = ExtractOneHot(["a","b"], "name", "count");
+julia> e(vs).data
+3×1 SparseArrays.SparseMatrixCSC{Int64,Int64} with 2 stored entries:
+  [1, 1]  =  1
+  [2, 1]  =  2	
+```
+
+	If `v` is equal to `nothing`, then it boils down to one-hot encoding
+```juliadoctest
+julia> e = ExtractOneHot(["a","b"], "name", nothing);
+julia> e(vs).data
+3×1 SparseArrays.SparseMatrixCSC{Int64,Int64} with 2 stored entries:
+  [1, 1]  =  1
+  [2, 1]  =  1	
+```
+
+	If there is key in the data which is not known (it was not part of `vs`),
+	than it is assigned to an special designed key serving as "unknown`
+```juliadoctest
+julia> vs = JSON.parse.(["{\"name\": \"c\", \"count\" : 1}"]);
+julia> e = ExtractOneHot(["a","b"], "name", nothing);
+julia> e(vs).data
+3×1 SparseArrays.SparseMatrixCSC{Bool,Int64} with 1 stored entry:
+  [3, 1]  =  1
+
+```
 """
 struct ExtractOneHot{K,I,V} <: AbstractExtractor
 	k::K
@@ -15,8 +48,8 @@ end
 dimension(e::ExtractOneHot) = e.n
 extractsmatrix(s::ExtractOneHot) = false
 
-function ExtractOneHot(vs::V, k, v = nothing) where {V<:Union{Vector, Set}}
-	key2id = Dict(zip(vs, 1:length(vs)))
+function ExtractOneHot(ks::V, k, v = nothing) where {V<:Union{Vector, Set}}
+	key2id = Dict(zip(ks, 1:length(ks)))
 	ExtractOneHot(k, v, key2id, length(key2id) + 1)
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,6 @@ using JsonGrinder
 using Test
 
 include("reflector.jl")
+include("extractonehot.jl")
 include("ngrams.jl")
 include("pipelinetest.jl")
