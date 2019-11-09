b5653d2868b2424554c070e8e78a91de4ab9c58c
Pevnak <pevnak@gmail.com>
2019-11-09 18:32:11 +0100

    fixed incorrect documentation in MultipleRepresentation

--- src/extractors/multirepresentation.jl
+++ src/extractors/multirepresentation.jl
@@ -1,10 +1,10 @@
-```
+"""
 	MultipleRepresentation(extractors::Tuple)
 
-	crate a 'ProductNode' where each item is the json part 
+	crate a `ProductNode` where each item is the json part 
 	processed by all extractors in the order
 
-```
+"""
 struct MultipleRepresentation{E<:Tuple}
 	extractors::E
 end
