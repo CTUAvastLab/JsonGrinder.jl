b0f9870fb53ef724b21a10763dd6b8ed98602dbc
Račinský Matěj <matej.racinsky@avast.com>
2020-01-17 09:25:46 +0100

    added test for showing html

--- test/show_html.jl
+++ test/show_html.jl
@@ -1,14 +1,22 @@
 using JsonGrinder, JSON, Test, SparseArrays
 
-@testset "ExtractMultipleRepresentation" begin
-	ex = MultipleRepresentation((ExtractCategorical(["Olda", "Tonda", "Milda"]),
-		JsonGrinder.ExtractString(String)))
-	e = ex("Olda")
+@testset "Generating HTML Schema" begin
+	j1 = JSON.parse("""{"a": [{"a":1},{"b":2}]}""")
+	j2 = JSON.parse("""{"a": [{"a":1,"b":3},{"b":2,"a":1}]}""")
+	j3 = JSON.parse("""{"a": [{"a":2,"b":3}]}""")
+	j4 = JSON.parse("""{"a": []}""")
+	j5 = JSON.parse("""{}""")
+	j6 = JSON.parse("""{"a": [{"a":1,"b":3},{"b":2,"a":1}], "b":1}""")
+	j7 = JSON.parse("""{"a": [{"a":4,"b":5},{"b":6,"a":7}], "b":2}""")
+	j8 = JSON.parse("""{"a": [{"a":9,"b":10},{"b":11,"a":12}], "b":2}""")
+	j9 = JSON.parse("""{"a": [{"a":4,"b":3},{"b":2,"a":2}], "b":2}""")
+	j10 = JSON.parse("""{"a": [{"a":11,"b":12},{"b":13,"a":14}], "b":2}""")
+	j11 = JSON.parse("""{"a": [{"a":7,"b":5},{"b":6,"a":6}], "b":2}""")
 
-	@test length(e.data) == 2
-	@test size(e.data[1].data) == (4, 1)
-	@test e.data[1].data[:] ≈ [0, 1, 0, 0]
-	@test size(e.data[2].data) == (2053, 1)
-	@test findall(x->x > 0, SparseMatrixCSC(e.data[2].data)) .|> Tuple == [(80, 1), (98, 1), (206, 1), (738, 1),
-		(1062, 1), (1856, 1)]
+	JsonGrinder.updatemaxkeys!(6)
+
+	sch = JsonGrinder.schema([j1,j2,j3,j4,j5,j6,j7,j8,j9,j10,j11])
+
+	JsonGrinder.generate_html(sch, "schema_test_maxvals=5.html", max_vals=5)
+	@test isfile("schema_test_maxvals=5.html")
 end
