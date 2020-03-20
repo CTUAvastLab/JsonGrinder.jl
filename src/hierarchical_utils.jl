9b05720382f6a5cd14af8b1c55fd0785d5894ba2
Matěj Račinský <matej.racinsky@avast.com>
2020-03-20 18:44:06 +0100

    upgraded dependencies for examples

--- src/hierarchical_utils.jl
+++ src/hierarchical_utils.jl
@@ -41,32 +41,3 @@ childrenfields(::Type{T}) where T <: MultipleRepresentation = (:extractors,)
 children(n::ExtractArray) = (n.item,)
 children(n::ExtractBranch) = (; Dict(Symbol(k)=>v for (k,v) in merge(filter(!isnothing, [n.vec, n.other])...))...)
 children(n::MultipleRepresentation) = n.extractors
-
-# Base.getindex(x::AbstractNode, i::Int) = x[i:i]
-#
-# # by šimon from Mill
-# NodeType(::Type{<:Union{ArrayNode, ArrayModel, BagNode{Missing}}}) = LeafNode()
-# NodeType(::Type{<:AbstractNode}) = InnerNode()
-# NodeType(::Type{<:AbstractBagNode}) = SingletonNode()
-# NodeType(::Type{<:BagModel}) = SingletonNode()
-# NodeType(::Type{<:MillModel}) = InnerNode()
-#
-# noderepr(n::ArrayNode) = "ArrayNode$(size(n.data))"
-# noderepr(n::ArrayModel) = "ArrayModel($(n.m))"
-# noderepr(n::BagNode) = "BagNode with $(length(n.bags)) bag(s)"
-# noderepr(n::BagModel) = "BagModel ↦ $(repr("text/plain", n.a)) ↦ $(repr("text/plain", n.bm))"
-# noderepr(n::WeightedBagNode) = "WeightedNode with $(length(n.bags)) bag(s) and weights Σw = $(sum(n.weights))"
-# noderepr(n::AbstractTreeNode) = "TreeNode"
-# noderepr(n::ProductModel) = "ProductModel ↦ $(noderepr(n.m))"
-#
-# childrenfield(::Type{<:Union{AbstractTreeNode, AbstractBagNode}}) = :data
-# childrenfield(::Type{BagModel}) = :im
-# childrenfield(::Type{ProductModel}) = :ms
-#
-# children(n::AbstractBagNode) = (n.data,)
-# children(n::BagModel) = (n.im,)
-# children(n::TreeNode) = n.data
-# children(n::ProductModel) = n.ms
-#
-# childrenstring(n::TreeNode{<:NamedTuple}) = ["$k: " for k in keys(n.data)]
-# childrenstring(n::ProductModel{<:NamedTuple}) = ["$k: " for k in keys(n.ms)]
