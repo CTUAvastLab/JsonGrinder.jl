c078dfad03f531a34858a0e533fd789821412c35
Pevnak <pevnak@gmail.com>
2020-04-15 14:46:58 +0200

    support of flexible schema

--- src/schema/scalar.jl
+++ src/schema/scalar.jl
@@ -91,3 +91,5 @@ Base.:(==)(e1::Entry, e2::Entry) = e1.updated === e2.updated && e1.counts == e2.
 sample_synthetic(e::Entry) = first(keys(e.counts))
 
 
+NodeType(::Type{T}) where {T<:Entry} = LeafNode()
+noderepr(n::Entry) = "[Scalar - $(join(sort(string.(types(n))), ","))], $(length(keys(n.counts))) unique values, updated = $(n.updated)"
