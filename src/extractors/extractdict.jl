93a7561bccc5bdbdcd704267d0d3efe95de55f1a
Matěj Račinský <matej.racinsky@avast.com>
2020-10-21 12:00:38 +0200

    first iteration of big refactoring, dropping scalar merging and single key special handling

--- src/extractors/extractdict.jl
+++ src/extractors/extractdict.jl
@@ -1,26 +1,22 @@
 
 """
 	struct ExtractDict
-		vec::Dict{String,Any}
-		other::Dict{String,Any}
+		dict::Dict{Symbol,Any}
 	end
 
 	extracts all items in `vec` and in `other` and return them as a ProductNode.
 """
-struct ExtractDict{S,V} <: AbstractExtractor
-	vec::S
-	other::V
-	function ExtractDict(v::S,o::V) where {S<:Union{Dict,Nothing},V<:Union{Dict,Nothing}}
-		v = (v == nothing || isempty(v)) ? nothing : v
-		o = (o == nothing || isempty(o)) ? nothing : o
-		new{typeof(v),typeof(o)}(v,o)
+struct ExtractDict{S} <: AbstractExtractor
+	dict::S
+	function ExtractDict(d::S) where {S<:Union{Dict,Nothing},V<:Union{Dict,Nothing}}
+		d = (isnothing(d) || isempty(d)) ? nothing : d
+		new{typeof(d)}(d)
 	end
 end
 
 
 function Base.getindex(m::ExtractDict, s::Symbol)
-	m.vec != nothing && haskey(m.vec, s) && return(m.vec[s])
-	m.other != nothing && haskey(m.other, s) && return(m.other[s])
+	!isnothing(m.dict) && haskey(m.dict, s) && return m.dict[s]
 	nothing
 end
 
@@ -31,29 +27,12 @@ extractsmatrix(s::ExtractDict) = false
 (s::ExtractDict)(v)  = s(nothing)
 
 
-function (s::ExtractDict{S,V})(v::Dict) where {S<:Dict,V<:Dict}
-	x = vcat([f(get(v,String(k),nothing)) for (k,f) in s.vec]...)
-	o = [Symbol(k) => f(get(v,String(k),nothing)) for (k,f) in s.other]
-	data = (; :scalars => x,o...)
-	ProductNode(data, [collect(keys(s.vec))])
+function (s::ExtractDict{S})(v::Dict) where {S<:Dict}
+	o = [Symbol(k) => f(get(v,String(k),nothing)) for (k,f) in s.dict]
+	ProductNode((; o...))
 end
-
-(s::ExtractDict{S,V})(v::Dict) where {S<:Dict,V<:Nothing} = vcat([f(get(v,k,nothing)) for (k,f) in s.vec]...)
-
-function (s::ExtractDict{S,V})(v::Dict) where {S<:Nothing,V<:Dict}
-	# o = [f(get(v,k,nothing)) for (k,f) in s.other]
-	o = [Symbol(k) => f(get(v,String(k),nothing)) for (k,f) in s.other]
-	if length(o) == 1
-		# return(o[1])
-		return o[1].second
-	else
-		return ProductNode((;o...))
-	end
-	ProductNode((;o...))
-end
-
 
 extractbatch(extractor, samples) = reduce(catobs, map(s-> extractor(s), samples))
 
-Base.hash(e::ExtractDict, h::UInt) = hash((e.vec, e.other), h)
-Base.:(==)(e1::ExtractDict, e2::ExtractDict) = e1.vec == e2.vec && e1.other == e2.other
+Base.hash(e::ExtractDict, h::UInt) = hash(e.dict, h)
+Base.:(==)(e1::ExtractDict, e2::ExtractDict) = e1.dict == e2.dict
