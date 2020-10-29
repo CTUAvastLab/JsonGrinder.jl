7f93176b851796295dc017dd33cc57604dd2c03d
Matěj Račinský <matej.racinsky@avast.com>
2020-10-29 17:36:52 +0100

    Returning to usage of named tuple for extractor, because extractor should be more stable than schema. Adding more tests for variours iterations. Using newer HUtils

--- test/pipelinetest.jl
+++ test/pipelinetest.jl
@@ -1,4 +1,4 @@
-using Mill, JSON, Flux, JsonGrinder, Test
+using Mill, JSON, Flux, JsonGrinder, Test, HierarchicalUtils
 
 using JsonGrinder: DictEntry, suggestextractor, schema
 using Mill: reflectinmodel
@@ -40,7 +40,7 @@ end
 	end
 end
 
-
+# todo: dodělat test pro navrhnutý model a zkontrolovat, že je ok, i s missingy a jedním klíčem v dictu a se scalary
 @testset "testing pipeline with arrays of dicts and missing values" begin
 	j1 = JSON.parse("""{"a": [{"a":1},{"b":2}]}""")
 	j2 = JSON.parse("""{"a": [{"a":1,"b":3},{"b":2,"a" : 1}]}""")
@@ -57,4 +57,81 @@ end
 	for i in 1:length(dss)
 		@test o[:,i] ≈ m(dss[i]).data
 	end
+end
+
+@testset "testing pipeline with single-keyed dicts and merging scalars" begin
+	j1 = JSON.parse("""{"a": [{"a":1},{"b":2,"c":"oh"}]}""")
+	j2 = JSON.parse("""{"a": [{"a":1,"b":3,"c":"hi"},{"b":2,"a":1,"c":"Mark"}]}""")
+	j3 = JSON.parse("""{"a": [{"a":2,"b":3}]}""")
+	j4 = JSON.parse("""{"a": []}""")
+	j5 = JSON.parse("""{}""")
+
+	sch = JsonGrinder.schema([j1,j2,j3,j4, j5])
+	ext = suggestextractor(sch)
+	dss = ext.([j1,j2,j3,j4,j5])
+	ds = reduce(catobs, dss)
+	m = reflectinmodel(ds, k -> Dense(k,10, relu))
+
+	# todo: otestovat
+	@test NodeIterator(m, ds) |> collect == [
+		(m[""], ds[""]),
+		(m["U"], ds["U"]),
+		(m["k"], ds["k"]),
+		(m["o"], ds["o"]),
+		(m["s"], ds["s"]),
+		(m["w"], ds["w"]),
+	]
+	@test NodeIterator(sch, ext) |> collect == [
+		(sch[""], ext[""]),
+		(sch["U"], ext["U"]),
+		(sch["k"], ext["k"]),
+		(sch["o"], ext["o"]),
+		(sch["s"], ext["s"]),
+		(sch["w"], ext["w"]),
+	]
+	@test NodeIterator(sch, m) |> collect == [
+		(sch[""], m[""]),
+		(sch["U"], m["U"]),
+		(sch["k"], m["k"]),
+		(sch["o"], m["o"]),
+		(sch["s"], m["s"]),
+		(sch["w"], m["w"]),
+	]
+	@test NodeIterator(ext, m) |> collect == [
+		(ext[""], m[""]),
+		(ext["U"], m["U"]),
+		(ext["k"], m["k"]),
+		(ext["o"], m["o"]),
+		(ext["s"], m["s"]),
+		(ext["w"], m["w"]),
+	]
+	@test buf_printtree(sch) == """
+	[Dict] (updated = 5)
+	  └── a: [List] (updated = 3)
+	           └── [Dict] (updated = 5)
+	                 ├── a: [Scalar - Int64], 2 unique values, updated = 4
+	                 ├── b: [Scalar - Int64], 2 unique values, updated = 4
+	                 └── c: [Scalar - String], 3 unique values, updated = 3"""
+	@test buf_printtree(ext) == """
+	Dict
+	  └── a: Array of
+	           └── Dict
+	                 ├── a: Float32
+	                 ├── b: Float32
+	                 └── c: String"""
+	@test buf_printtree(m) == """
+	ProductModel ↦ ArrayModel(identity)
+	  └── a: BagModel ↦ ⟨SegmentedMean(10)⟩ ↦ ArrayModel(Dense(11, 10, relu))
+	           └── ProductModel ↦ ArrayModel(Dense(12, 10, relu))
+	                 ├── a: ArrayModel(identity)
+	                 ├── b: ArrayModel(identity)
+	                 └── c: ArrayModel(Dense(2053, 10, relu))"""
+	@test buf_printtree(ds) == """
+	ProductNode
+	  └── a: BagNode with 5 bag(s)
+	           └── ProductNode
+	                 ├── a: ArrayNode(1, 5)
+	                 ├── b: ArrayNode(1, 5)
+	                 └── c: ArrayNode(2053, 5)"""
+
 end
