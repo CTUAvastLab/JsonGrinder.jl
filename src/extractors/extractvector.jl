154f659ab759253458f28ec5c8a1d6a87ce8546a
Pevnak <pevnak@gmail.com>
2020-12-08 21:18:11 +0100

    Fixed the bug. Found a new failinng test in last item of pipelinetest

--- src/extractors/extractvector.jl
+++ src/extractors/extractvector.jl
@@ -21,7 +21,7 @@ ExtractVector(n::Int) = ExtractVector{FloatType}(n)
 
 extractsmatrix(s::ExtractVector) = false
 # todo: dodělat missingy, všchny nothing předělat na missing a pořádně to otestovat
-(s::ExtractVector{T})(::V) where {T,V<:Union{Missing, Nothing}} = ArrayNode(fill(missing, s.n))
+(s::ExtractVector{T})(::V) where {T,V<:Union{Missing, Nothing}} = ArrayNode(fill(missing, s.n, 1))
 (s::ExtractVector)(v) = s(missing)
 function (s::ExtractVector{T})(v::V) where {T,V<:Vector}
 	isempty(v) && return s(missing)
