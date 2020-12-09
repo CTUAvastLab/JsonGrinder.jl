e8c7c5449cd51d242062bd1f0e9ef452721bf119
Pevnak <pevnak@gmail.com>
2020-12-09 06:12:34 +0100

    added a singleton for signaling that an empty arrays should be extracted

---
+++ src/extractors/extractors.jl
@@ -0,0 +1,19 @@
+struct ExtractEmpty end
+
+"""
+    extractempty
+	A singleton of type [`ExtractEmpty`](@ref) is used to signal 
+	downstream extractors that they should extract an Empty Array
+"""
+const extractempty = ExtractEmpty()
+
+include("auxiliary.jl")
+include("extractarray.jl")
+include("extractdict.jl")
+include("extractcategorical.jl")
+include("extractscalar.jl")
+include("extractstring.jl")
+include("extractvector.jl")
+include("extractonehot.jl")
+include("extract_keyasfield.jl")
+include("multirepresentation.jl")
