0786acd177cc4b390210a9a521962d8b9ff3edbf
Matěj Račinský <matej.racinsky@avast.com>
2021-01-28 13:17:13 +0100

    updated recipes example

--- examples/recipes.jl
+++ examples/recipes.jl
@@ -40,8 +40,6 @@ data = reduce(catobs, data)
 target = tmap(extract_target, samples[1:5_000])
 target = reduce(catobs, target)[:cuisine].data
 
-e = sch[:cuisine]
-
 ###############################################################
 # 	create the model according to the data
 ###############################################################
@@ -51,19 +49,6 @@ m = reflectinmodel(sch, extract_data,
 	# b = Dict("" => k -> Dense(k, size(target, 1))),
 	fsm = Dict("" => k -> Dense(k, size(target, 1))),
 )
-
-ex = extract_data
-full_sample = ex(JsonGrinder.sample_synthetic(sch, empty_dict_vals=false))
-leaves_empty_sample = ex(JsonGrinder.sample_synthetic(sch, empty_dict_vals=true))
-specimen = catobs(full_sample, leaves_empty_sample)
-m2 = reflectinmodel(specimen,
-	k -> Dense(k,20,relu),
-	d -> SegmentedMeanMax(d),
-	fsm = Dict("" => k -> Dense(k, 21)),
-	fsa = Dict(),
-	single_key_identity=true, single_scalar_identity=true
-)
-printtree(m2, trav=true)
 
 ###############################################################
 #  train
@@ -83,9 +68,10 @@ ps = Flux.params(m)
 mean(Flux.onecold(m(data).data) .== Flux.onecold(target))
 
 @info "testing the gradient"
+loss(data, target)
 gs = gradient(() -> loss(data, target), ps)
 Flux.Optimise.update!(opt, ps, gs)
-loss(m(data).data, target)
+
 Flux.Optimise.train!(loss, ps, repeatedly(() -> (data, target), 20), opt, cb = Flux.throttle(cb, 2))
 # feel free to train for longer period of time, this example is learns only 20 iterations, so it runs fast
 # Flux.Optimise.train!(loss, ps, repeatedly(() -> (data, target), 500), opt, cb = Flux.throttle(cb, 10))
