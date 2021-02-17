3137bdd723ef63241a832d2c49b9b8cbb1cc4c9f
Matěj Račinský <matej.racinsky@avast.com>
2021-02-17 23:43:33 +0100

    fixed some tests

--- src/extractors/extract_keyasfield.jl
+++ src/extractors/extract_keyasfield.jl
@@ -12,7 +12,7 @@ struct ExtractKeyAsField{S,V} <: AbstractExtractor
 	item::V
 end
 
-function (e::ExtractKeyAsField)(v::V) where {V<:Union{Missing,Nothing}}
+function (e::ExtractKeyAsField)(v::MissingOrNothing)
 	Mill._emptyismissing[] && return BagNode(missing, [0:-1])
 	BagNode(ProductNode((key = e.key(extractempty), item = e.item(extractempty)))[1:0], [0:-1])
 end
