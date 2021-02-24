8667b81699731657c124f54370aa235af42d8fee
Matěj Račinský <matej.racinsky@avast.com>
2021-02-24 19:25:14 +0100

    split tests to testsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,13 +18,21 @@ const ≃ = isequal
     	doctest(JsonGrinder)
 	end
 
-	include("extractors.jl")
-	include("multirepresentation.jl")
-	include("pipelinetest.jl")
-	include("schema.jl")
+	@testset "Extractors" begin
+		include("extractors.jl")
+		include("multirepresentation.jl")
+	end
+	@testset "Pipeline" begin
+		include("pipelinetest.jl")
+	end
+	@testset "Schema" begin
+		include("schema.jl")
+	end
+	@testset "HierarchicalUtils" begin
+		include("hierarchical_utils_extractors.jl")
+		include("hierarchical_utils_schema.jl")
+	end
 	include("show_html.jl")
-	include("hierarchical_utils_extractors.jl")
-	include("hierarchical_utils_schema.jl")
 	include("util.jl")
 
 end
