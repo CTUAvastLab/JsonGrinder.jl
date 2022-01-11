09541d580214bc0c0e55e6b377c693b9c263ab2d
Matěj Račinský <matej.racinsky@avast.com>
2022-01-11 13:22:03 +0100

    better ordering inside example, added generation of workflow

--- docs/draw_diagram.jl
+++ docs/draw_diagram.jl
@@ -1,1 +1,19 @@
 using GraphViz
+
+# generating schema of the workflow
+g = GraphViz.Graph("""
+digraph graphname {
+   node [shape=box];
+   label = "Basic workshow of Mill and JsonGrinder"
+   a  [label="step 1\ncreate schema"]
+   b  [label="step 2\ncreate extractor from schema"]
+   c  [label="step 3\ncreate model from schema and extractor"]
+   d  [label="step 4\nprepare training data using the extractor"]
+   e  [label="step 5\ntrain the model"]
+    a -> b -> c -> d -> e;
+}
+""");
+GraphViz.layout!(g, engine="dot")
+open(joinpath(@__DIR__, "src", "assets", "workflow.svg"), "w+") do io
+    GraphViz.render(io, g)
+end
