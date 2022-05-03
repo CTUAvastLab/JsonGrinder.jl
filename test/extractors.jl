5a02633906bbea16c815bb1435e724b79f86d628
Matěj Račinský <matej.racinsky@avast.com>
2022-05-03 18:08:58 +0200

    fiddling with version conditioning

--- test/extractors.jl
+++ test/extractors.jl
@@ -612,8 +612,13 @@ end
 		
 		e = suggestextractor(sch)
 		@test e(d1) != e(d2)
-		@test e(d1)[:key].data ≈ [1,0,0]
-		@test e(d2)[:key].data ≈ [0,1,0]
+		@static if VERSION < v"1.7.0-"
+			@test e(d1)[:key].data ≈ [0,1,0]
+			@test e(d2)[:key].data ≈ [1,0,0]
+		else
+			@test e(d1)[:key].data ≈ [1,0,0]
+			@test e(d2)[:key].data ≈ [0,1,0]
+		end
 		
 		e2 = suggestextractor(sch)
 		@set! e2.dict[:key] = JsonGrinder.extractscalar(String, sch[:key])
