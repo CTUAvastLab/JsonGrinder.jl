ec2079ecb461a641f8b16080ca86081095c692b0
Matěj Račinský <matej.racinsky@avast.com>
2022-05-03 17:33:49 +0200

    fixing the rand numbers

--- test/extractors.jl
+++ test/extractors.jl
@@ -612,8 +612,8 @@ end
 		
 		e = suggestextractor(sch)
 		@test e(d1) != e(d2)
-		@test e(d1)[:key].data ≈ [0,1,0]
-		@test e(d2)[:key].data ≈ [1,0,0]
+		@test e(d1)[:key].data ≈ [1,0,0]
+		@test e(d2)[:key].data ≈ [0,1,0]
 		
 		e2 = suggestextractor(sch)
 		@set! e2.dict[:key] = JsonGrinder.extractscalar(String, sch[:key])
